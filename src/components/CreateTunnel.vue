04accd85029fd19415ae3744672a090ec13a6431
mma <mma@pst.net>
2025-09-25 18:55:30 +0300

    fix: Add scroll to creating tunnel

--- src/components/CreateTunnel.vue
+++ src/components/CreateTunnel.vue
@@ -62,92 +62,105 @@ onMounted(async () => {
 </script>
 
 <template>
-  <div class="w-full flex items-center justify-center">
+  <div class="w-full h-full flex items-center justify-center">
     <div
-      class="w-1/3 h-1/6 flex flex-col justify-center items-center content-center"
+      class="w-1/3 h-full flex flex-col justify-start items-center content-center min-w-96"
     >
-      <h1 class="text-center text-large font-600 font-bold mb-4">
-        Create Tunnel
-      </h1>
-      <el-input-number
-        max="65535"
-        min="0"
-        class="mb-1"
-        v-model="port"
-        placeholder="Port"
-      />
-      <el-button
-        class="my-3"
-        :icon="advanced ? Top : Bottom"
-        type="primary"
-        @click="advanced = !advanced"
-        text
-      >
-        {{
-          advanced ? "Hide advanced options" : "Show advanced options"
-        }}</el-button
-      >
+      <el-scrollbar class="w-full h-full">
+        <div class="px-6 py-4">
+          <h1 class="text-center text-2xl font-bold mb-6">Create Tunnel</h1>
+          <el-input-number
+            max="65535"
+            min="0"
+            class="mb-3 w-full"
+            v-model="port"
+            placeholder="Port"
+          />
+          <el-button
+            class="my-4"
+            :icon="advanced ? Top : Bottom"
+            type="primary"
+            @click="advanced = !advanced"
+            text
+          >
+            {{ advanced ? "Hide advanced options" : "Show advanced options" }}
+          </el-button>
 
-      <el-collapse-transition>
-        <div v-show="advanced">
-          <el-select
-            v-model="domain"
-            filterable
-            allow-create
-            default-first-option
-            placeholder="Domain (optional)"
-            :reserve-keyword="false"
-            class="mb-1"
+          <el-collapse-transition>
+            <div v-show="advanced" class="w-full">
+              <el-select
+                v-model="domain"
+                filterable
+                allow-create
+                default-first-option
+                placeholder="Domain (optional)"
+                :reserve-keyword="false"
+                class="mb-3 w-full"
+              >
+                <template #prefix>https://</template>
+                <el-option
+                  v-for="item in staticDomains"
+                  :key="item"
+                  :label="item"
+                  :value="item"
+                />
+              </el-select>
+              <el-input
+                class="mb-3 w-full"
+                type="text"
+                v-model="host_rewrite"
+                placeholder=" Host Rewrite (optional)"
+              >
+                <template #prefix>https://</template>
+              </el-input>
+
+              <el-divider />
+              <el-text class="text-sm font-medium text-gray-700"
+                >Basic Authentication</el-text
+              >
+              <el-input
+                class="mb-3 mt-2 w-full"
+                type="text"
+                v-model="username"
+                placeholder="Username"
+              />
+              <el-input
+                class="mb-3 w-full"
+                type="password"
+                show-password
+                v-model="password"
+                placeholder="Password"
+              />
+              <el-divider />
+              <el-text class="text-sm font-medium text-gray-700"
+                >Request Headers</el-text
+              >
+              <div class="mb-4 mt-2">
+                <HeadersEditor v-model="requestHeaders" :max-headers="9" />
+              </div>
+
+              <el-divider />
+              <el-text class="text-sm font-medium text-gray-700"
+                >Response Headers</el-text
+              >
+              <div class="mb-4 mt-2">
+                <HeadersEditor v-model="responseHeaders" :max-headers="9" />
+              </div>
+            </div>
+          </el-collapse-transition>
+          <el-button
+            class="mt-6 mb-4 w-full"
+            type="primary"
+            size="large"
+            :icon="Plus"
+            round
+            @click="createTunnel"
+            :loading="isLoading"
           >
-            <template #prefix>https://</template>
-            <el-option
-              v-for="item in staticDomains"
-              :key="item"
-              :label="item"
-              :value="item"
-            />
-          </el-select>
-          <el-input
-            class="mb-1"
-            type="text"
-            v-model="host_rewrite"
-            placeholder=" Host Rewrite (optional)"
-          >
-            <template #prefix>https://</template>
-          </el-input>
-          <el-divider />
-          <el-text>Request Headers</el-text>
-          <HeadersEditor v-model="requestHeaders" :max-headers="9" />
-          <el-divider />
-          <el-text>Response Headers</el-text>
-          <HeadersEditor v-model="responseHeaders" :max-headers="9" />
-          <el-divider />
-          <el-text>Basic Authentication</el-text>
-          <el-input
-            class="mb-1"
-            type="text"
-            v-model="username"
-            placeholder="Username"
-          />
-          <el-input
-            class="mb-1"
-            type="password"
-            show-password
-            v-model="password"
-            placeholder="Password"
-          />
+            Create
+          </el-button>
         </div>
-      </el-collapse-transition>
-      <el-button
-        class="my-4 w-full"
-        type="primary"
-        size="large"
-        :icon="Plus"
-        round
-        @click="createTunnel"
-        :loading="isLoading"
-        >Create</el-button
-      >
+      </el-scrollbar>
     </div>
   </div>
 </template>
--- src/pages/TunnelCreate.vue
+++ src/pages/TunnelCreate.vue
@@ -4,16 +4,20 @@ import { ArrowLeft } from "@element-plus/icons-vue";
 </script>
 
 <template>
-  <div class="flex justify-between w-full">
-    <el-button
-      :icon="ArrowLeft"
-      tag="router-link"
-      round
-      plain
-      :to="{ name: 'tunnel-list' }"
-      class="m-5"
-      >Back to tunnel list</el-button
-    >
+  <div class="h-screen flex flex-col">
+    <div class="flex justify-between w-full flex-shrink-0">
+      <el-button
+        :icon="ArrowLeft"
+        tag="router-link"
+        round
+        plain
+        :to="{ name: 'tunnel-list' }"
+        class="m-5"
+        >Back to tunnel list</el-button
+      >
+    </div>
+    <div class="flex-1 overflow-hidden">
+      <CreateTunnel />
+    </div>
   </div>
-  <CreateTunnel />
 </template>
